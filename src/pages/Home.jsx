ddeb0be0fdb6708d1310fe351625bb27e7760559
imam <crazywar19@gmail.com>
2025-07-15 20:07:23 +0700

    Initial commit: React + Tailwind + Router + Navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="p-4">
+        <h1 className="text-3xl font-bold">Halaman Home</h1>
+        <p>Selamat datang di React + Tailwind!</p>
+      </div>
+    );
+  }
+
